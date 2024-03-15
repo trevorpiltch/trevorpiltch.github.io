88283f802c5003a43cbc267934fd934b4c1a787a
Trevor Piltch <trevor@piltch.com>
2024-03-14 22:30:34 -0400

    Fixed work section and added some other details (dots!)

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 window.onload = function () {
     var header = document.getElementById('header');
     var content = document.getElementById('content');
+    var photo = document.getElementById('profile-photo');
 
     // Get the height of the header
     var headerHeight = header.offsetHeight;
 
     // Set the top margin of the content to the height of the header
-    content.style.marginTop = 1.15  * headerHeight + 'px';
+    photo.style.marginTop = headerHeight + 'px';
+    content.style.marginTop =  headerHeight + 'px';
 }
