2ecc1a057dbf9e999775fd244ea0b52978269d28
Trevor Piltch <trevor@piltch.com>
2024-03-16 00:51:23 -0400

    add most of v2

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ window.onload = function () {
     photo.style.marginTop = headerHeight + 'px';
     content.style.marginTop =  headerHeight + 'px';
 }
+
+$(function () {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+var links = ['about', 'work', 'labs', 'words'];
+
+links.forEach(function (link) {
+    document.querySelector('#' + link + '-link').addEventListener('click', function (e) {
+        e.preventDefault();
+        var headerHeight = document.querySelector('#header').offsetHeight;
+        var target = document.querySelector('#' + link);
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        window.scrollBy(0, -headerHeight);
+    });
+});
